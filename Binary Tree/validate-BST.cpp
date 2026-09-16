f064a934f1e23015529b71afc207b9369591cda4
DEEPAK R <rajkumardeepak2005@gmail.com>
2026-09-16 13:44:49 +0530

    Implement BST validation in validate-BST.cpp

---
+++ Binary Tree/validate-BST.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValidBST(TreeNode* root,long long min,long long max)
+    {
+        if(root==NULL)
+        {
+            return true;
+        }
+        if(root->val>=max||root->val<=min) return false;
+        return isValidBST(root->left,min,root->val)&&isValidBST(root->right,root->val,max);
+    }
+    bool isValidBST(TreeNode* root) {
+       return isValidBST(root,LONG_MIN,LONG_MAX);
+        
+        
+    }
+};
