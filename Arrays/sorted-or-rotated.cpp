aed23dae889c06c2c56b87ef3a22e38a4752c681
DEEPAK R <rajkumardeepak2005@gmail.com>
2026-09-17 16:34:01 +0530

    Create sorted-or-rotated.cpp

---
+++ Arrays/sorted-or-rotated.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int count=0;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i-1]>nums[i])
+            {
+                count++;
+            }
+        }
+        if(nums[nums.size()-1]>nums[0])
+        {
+            count++;
+        }
+        if(count<=1)
+        {
+            return true;
+        }
+        return false;
+        
+    }
+};
+
+
+/*
+Example 1:
+
+Input: nums = [3,4,5,1,2]
+Output: true
+Explanation: [1,2,3,4,5] is the original sorted array.
+You can rotate the array by x = 2 positions to begin on the element of value 3: [3,4,5,1,2].
+Example 2:
+
+Input: nums = [2,1,3,4]
+Output: false
+Explanation: There is no sorted array once rotated that can make nums.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: true
+Explanation: [1,2,3] is the original sorted array.
+You can rotate the array by x = 0 positions (i.e. no rotation) to make nums.
+*/
