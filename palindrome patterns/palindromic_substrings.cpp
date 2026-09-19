e0e888fbdc1d24eada9dcb25af836201f10bbc50
DEEPAK R <rajkumardeepak2005@gmail.com>
2026-09-19 16:25:42 +0530

    Add palindromic_substrings.cpp to count palindromes
    Implement a solution to count palindromic substrings in a given string using an expand-around-center approach.

---
+++ palindrome patterns/palindromic_substrings.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int expand(string &s,int left,int right)
+    {
+        int count=0;
+        while(left>=0&&right<=s.length()&&s[left]==s[right])
+        {
+            count++;
+            left--;
+            right++;
+        }
+        return count;
+    }
+    int countSubstrings(string s) {
+        int count=0;
+        int start=0;
+        int end=0;
+        for(int i=0;i<s.length();i++)
+        {
+           count+=expand(s,i,i);
+           count+=expand(s,i,i+1);
+        }
+        return count;
+        
+    }
+};
+/*
+Given a string s, return the number of palindromic substrings in it.
+
+A string is a palindrome when it reads the same backward as forward.
+
+A substring is a contiguous sequence of characters within the string.
+
+ 
+
+Example 1:
+
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+Example 2:
+
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+*/
