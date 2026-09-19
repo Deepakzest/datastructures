f138881a01e40520a736dd3292133b1f2fa946ce
DEEPAK R <rajkumardeepak2005@gmail.com>
2026-09-19 17:04:51 +0530

    Implement variable swap without third variable
    Added an alternative method to swap two variables without using a third variable.

--- swap-without-third-variable.cpp
+++ swap-without-third-variable.cpp
@@ -3,3 +3,10 @@ int a = 10, b = 20;
 a = a ^ b; // a becomes 30
 b = a ^ b; // b becomes 10
 a = a ^ b; // a becomes 20
+
+
+int a = 10, b = 20;
+
+a = a + b; // a becomes 30 (10 + 20)
+b = a - b; // b becomes 10 (30 - 20, original value of a)
+a = a - b; // a becomes 20 (30 - 10, original value of b)
