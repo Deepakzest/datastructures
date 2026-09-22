6709323ecbbdc7bc62f5ccec97fa7bd0423c5190
DEEPAK R <rajkumardeepak2005@gmail.com>
2026-09-23 01:02:44 +0530

    Update invert_trees.cpp

--- Binary Tree/invert_trees.cpp
+++ Binary Tree/invert_trees.cpp
@@ -15,3 +15,19 @@ public:
         
     }
 };
+
+/*class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root==NULL)
+        {
+            return nullptr;
+        }
+        swap(root->left,root->right);
+        invertTree(root->left);
+        invertTree(root->right);
+        return root;
+        
+    }
+};
+*/
